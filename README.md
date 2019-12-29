6e2c3939a687414b04467f6336c0ed34fb11e5f4
Colin <c3g6b2@gmail.com>
2019-12-28 19:02:23 -0500

    Working Version. opens, reads, renames. todo: modularize

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # pdf-renamer
-Search through a dir of PDFs for regex, rename pdf to the regex found
+* Do you have many PDFs you need to rename? 
+* Does the title exist in each of the PDFs?
+* Can you create a [regex pattern](https://www.regexr.com) that can find the title?
 
-TODO
+##Backstory
+
+A customer of mine had some data management software software that hashed the file names into some garbletygook, but the PDFs were simply renamed and moved into a folder. 
+
+inside the folder it would look like the ./1_sources directory in this project:
+
+```
+./1_sources
+----56md6asnm5.pdf
+----76ffy74cht.pdf
+----s5yhs5ys5y.pdf
+```
+
+So I wrote this script to: 
+1. Go into the sources folder for source PDFs
+2. Search through the text in the PDFs
+3. Finds titles with your regex
+4. For each file in your sources directory, copy a renamed file to output folder, each named according to your regex
+
--- renamer.py
+++ renamer.py
@@ -1,71 +1,122 @@
 import re
 import os
-import PyPDF4
+import shutil
 from PIL import Image
 import pytesseract
 import sys
-from pdf2image import convert_from_path
+from pdf2image import convert_from_path, convert_from_bytes
+import cProfile as cp
+
+# Requires "poppler". https://stackoverflow.com/a/53960829
+# poppler needs to be added to PATH
+
+# Requires "pytesseract". https://github.com/UB-Mannheim/tesseract/wiki
+# pytesseract needs to be added to PATH
+def main():
+    print(
+        'Welcome to PDFRenamer\n'
+        '1. Put source PDFs into ./1_sources directory\n'
+        '2. When the script runs, the OCRed text will be saved in ./3_textoutput/\n'
+        '3. If there\'s a regex match, the files will be re-saved to ./4_pdfoutput/\n'
+    )
+
+    # regex for searching for new file's name
+    regex = re.compile('(CB-[\d]{3,}-COL)')
+    # phone number
+    # regex = '^\([0-9]{3}\)[0-9]{3}-[0-9]{4}$'
+
+    inputdir = './1_sources/'
+    imagedir = './2_imageoutput/'
+    textdir = './3_textoutput/'
+    outputdir = './4_pdfoutput/'
+
+    # create all the directories above if they do not exist
+    if not os.path.exists(inputdir):
+        os.mkdir(inputdir)
+        print('Please put source pdfs into {}, then rerun program'.format(inputdir))
+        quit()
+    if not os.path.exists(imagedir):
+        os.mkdir(imagedir)
+    if not os.path.exists(textdir):
+        os.mkdir(textdir)
+    if not os.path.exists(outputdir):
+        os.mkdir(outputdir)
+
+    # get a list of everything in the directory to be RENAMED
+    pdf_input_list = next(os.walk(inputdir))[2]
+    # 1: Read PDF
+    # 2. Create Image from PDF
+    # 3. Create Text from image
+    # 4. Regex text
+    # 5. Rename PDFs from step 1 to regex result
+
+    # 1: Read PDF
+    # 2. Create Image from PDF
+    for filename in pdf_input_list:
+        out = open('{}{}.txt'.format(textdir, filename), 'w')
+        image_file_path = '{}{}'.format(imagedir, filename.replace('.pdf', ''))
+        pdf_file = open('{}{}'.format(inputdir, filename), 'rb')
+        pdf_file_path = '{}{}'.format(inputdir, filename)
+        # pages = convert_from_path(pdf_file, 500)
+        pages = convert_from_path(pdf_file_path, 200)
+        no_of_pages = len(pages)
+        for index, page in enumerate(pages):
+            img_path = image_file_path + '_page' + str(index + 1) + '.jpg'
+            page.save(img_path, 'JPEG')
+            text = str(pytesseract.image_to_string(Image.open(img_path)))
+            # text = text.replace('-\n', '')
+            out.write(text)
+        out.close()
+
+    # get a list of the directory we are getting the new names from
+    text_doc_list = next(os.walk(textdir))[2]
+    image_doc_list = next(os.walk(imagedir))[2]
+
+    # If you need to make of every file name, use this one
+    CONFIRM_EVERY_OPERATION = False
+
+    ## OK Here's the potentially destructive part. By Default, it's safe to run and nothing deletes.
+    # Change these vars to True to do what they imply
+    DELETE_SOURCES_WHEN_FINISHED = False
+    DELETE_TEXT_WHEN_FINISHED = False
+    DELETE_IMAGES_WHEN_FINISHED = False
 
 
+    # 4. read text, and 5. rename the PDFs
+    for filename in text_doc_list:
+        nextfile = textdir + filename
+        print('processing ' + nextfile.strip('.txt'))
+        with open(nextfile, 'r') as f:
+            ourtext = str(f.read())
+            f.close()
+            #  fails here if there's nothing found
+            try:
+                nextname = re.findall(regex, ourtext)[0]
+                print(nextname)
+            except IndexError:
+                print("No Matching Regex Found")
+                quit()
+            print('new name is: ' + nextname + '.pdf')
+            proceed = ''
+            if CONFIRM_EVERY_OPERATION:
+                proceed = input('Confirm? Y, N\n')
+            if (proceed == 'Y' or 'y') or CONFIRM_EVERY_OPERATION == False:
+                in_pdf_name = inputdir + filename.replace('.txt', '')
+                out_pdf_name = outputdir + nextname + '.pdf'
+                # os.rename(in_pdf_name, out_pdf_name)
+                shutil.copy(in_pdf_name, out_pdf_name)
 
-print(
-    'Welcome to PDFRenamer\n'
-    '1. Put source PDFs into ./1_sources directory\n'
-    '2. When the script runs, the OCRed text will be saved in ./3_textoutput/\n'
-    '3. If there\'s a regex match, the files will be re-saved to ./4_pdfoutput/\n'
-)
+            nextname = ''
 
-# test regex
-regex ='^(CB-[\d]{4}-COL)$'
+    if DELETE_IMAGES_WHEN_FINISHED:
+        for file in image_doc_list:
+            os.remove(imagedir + file)
+    if DELETE_TEXT_WHEN_FINISHED:
+        for file in text_doc_list:
+            os.remove(textdir + file)
+    if DELETE_SOURCES_WHEN_FINISHED:
+        for file in pdf_input_list:
+            os.remove(inputdir + file)
 
-# phone number
-# regex = '^\([0-9]{3}\)[0-9]{3}-[0-9]{4}$'
-inputdir = './1_sources/'
-imagedir = './2_imageoutput/'
-textdir = './3_textoutput/'
-outputdir = './4_pdfoutput/'
-
-# get a list of everything in the directory to be RENAMED
-original_doc_list = next(os.walk(inputdir))[2]
-# 1: Read PDF
-# 2. Create Image from PDF
-# 3. Create Text from image
-# 4. Regex text
-# 5. Rename PDFs from step 1 to regex result
-
-for filename in original_doc_list:
-    out = open('{}{}.txt'.format(textdir, filename), 'w')
-    out.seek(0)
-    out.truncate()
-    pdfFileObj = open('{}{}'.format(inputdir, filename), 'rb')
-    pages = convert_from_path(pdfFileObj,500)
-    no_of_pages = len(pages)
-    for page in pages:
-        page = pdfReader.getPage(page_no)
-        page_content = page.extractText()
-        page.save(filename, 'JPEG')
-    out.close()
-    pdfFileObj.close()
-
-# get a list of the directory we are getting the new names from
-text_doc_list = next(os.walk(textdir))[2]
-print(text_doc_list)
-
-for filename in text_doc_list:
-
-    nextfile = textdir + filename
-    print('processing ' + nextfile.strip('.txt'))
-    with open(nextfile, 'r') as f:
-        ourtext = str(f.read())
-        f.close()
-        #  fails here if there's nothing found
-        try:
-            nextname = re.findall(regex, ourtext)[0]
-        except IndexError:
-            print("No Matching Regex Found")
-            quit()
-        print('new name is: ' + nextname + '.pdf')
-        proceed = input('Confirm? Y, N\n')
-        if proceed == 'Y' or 'y':
-            os.rename(inputdir + filename + '.pdf', outputdir + nextname + '.pdf')
-        nextname = ''
+if __name__ == '__main__':
+    main()
